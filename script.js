195289f1df6b21c9d53babf68286d3f7b1da4c6f
Diego Vilella Rodrigues <divilella@gmail.com>
2024-12-01 21:07:47 +0000

    Gerar código de barras
    Com intuito de realizar o processo também de gerar o código de barras fizemos o deploy para que o mesmo faça a leitura e gere o código.

--- script.js
+++ script.js
@@ -2,10 +2,21 @@ document.getElementById('inputCodigo').addEventListener('input', function () {
   const input = this.value.replace(/\D/g, ''); // Remove tudo que não é número
   const regex = /^(\d{6})(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{4})(\d{4})(\d{4})$/;
 
+  // Formatar o código
   if (regex.test(input)) {
     const formatted = input.replace(regex, '$1 $2-$3-$4 $5:$6 $7 $8 $9');
     document.getElementById('codigoFormatado').textContent = formatted;
+
+    // Gerar código de barras
+    JsBarcode("#barcode", input, {
+      format: "CODE128",
+      lineColor: "#000",
+      width: 2,
+      height: 50,
+      displayValue: false,
+    });
   } else {
     document.getElementById('codigoFormatado').textContent = 'Formato inválido ou incompleto.';
+    document.getElementById('barcode').innerHTML = ''; // Limpa o código de barras
   }
 });
